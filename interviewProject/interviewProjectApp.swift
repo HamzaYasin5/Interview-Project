365048022f6502578cce986ff870f2729fff123b
Hamza Yasin <hamza@techificent.com>
2025-06-20 06:36:52 +0500

    Initial Commit

---
+++ interviewProject/interviewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  interviewProjectApp.swift
+//  interviewProject
+//
+//  Created by hamza yasin on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct interviewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
